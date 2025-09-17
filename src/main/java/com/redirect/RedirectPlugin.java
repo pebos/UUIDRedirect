4aa0e4884ac77bbb09c9ecf1c43d9a26ed25721c
ScriptKiddie <66707540+pebos@users.noreply.github.com>
2025-09-17 16:45:11 -0500

    Update RedirectPlugin.java

--- src/main/java/com/redirect/RedirectPlugin.java
+++ src/main/java/com/redirect/RedirectPlugin.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 @Plugin(
         id = "uuidredirect",
         name = "UUID Redirect",
-        version = "1.0.0",
+        version = "1.0.1",
         description = "Redirects specific UUIDs or usernames to pre-registered Velocity servers and prevents switching",
         authors = {"Owen Osborne"}
 )
